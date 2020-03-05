2dcce64dc79f6926fbe6b66ebf9883109451ea77
TheInfinity007 <ramchandra71999@gmail.com>
2020-03-05 14:56:13 +0530

    add the review/new.ejs

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 var express 			= require("express");
-var router 			= express.Router();
+var router 			= express.Router({mergeParams: true});
 var Campground = require("../models/campground");
 var Review			= require("../models/review");
 var middleware 	= require("../middleware");
@@ -7,13 +7,15 @@ var middleware 	= require("../middleware");
 
 
 // /*NEW ROUTE*/
-router.get("/new", middleware.isLoggedIn, (req, res)=>{
+router.get("/new", (req, res)=>{
 	//checkReviewExistence checks if a user already reviewed the campground, only one review per user is allowed.
 	Campground.findById(req.params.id, (err, campground)=>{
 		if(err){
 			req.flash("error", err.message);
 			return res.redirect("back");
 		}else{
+			console.log(req.params.id);
+			console.log(campground);
 			res.render("reviews/new", {campground: campground});
 		}
 	});
