9309f99c456fff9c4ff5e2c9d3c4f2f64aebac09
TheInfinity007 <ramchandra71999@gmail.com>
2020-03-06 17:05:32 +0530

    add a flash message

--- routes/comments.js
+++ routes/comments.js
@@ -52,6 +52,7 @@ router.get("/:comment_id/edit", middleware.checkCommentOwnership, (req, res)=>{
 		}
 		Comment.findById(req.params.comment_id, (err, foundComment)=>{
 			if(err){
+				req.flash("error", err.message);
 				res.redirect("back");
 			}else{
 				res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});		
