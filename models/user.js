5398287ccdadb91740a6a557df8757989fd45159
TheInfinity007 <ramchandra71999@gmail.com>
2020-03-17 19:34:09 +0530

    created notification model and updated user model

--- models/user.js
+++ models/user.js
@@ -11,7 +11,19 @@ var userSchema = new mongoose.Schema({
 	isAdmin: { type: Boolean, default: false	 },
 	description: { type: String, default: "" },
 	resetPasswordToken: String,
-	resetPasswordExpires: Date
+	resetPasswordExpires: Date,
+	notifications: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Notification"
+		}
+	],
+	followers: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}
+	]
 });
 
 userSchema.plugin(passportLocalMongoose);
