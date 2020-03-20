733e900caf4dd65ab0e4ef581b78c95c57a0b704
TheInfinity007 <ramchandra71999@gmail.com>
2020-03-21 00:21:31 +0530

    setup the user-edit for the image cloudinary image upload

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
 	username: {	type: String, 	unique: true, required: true	},
 	password: String,
-	avatar: { type: String, default: "https://upload.wikimedia.org/wikipedia/commons/6/67/User_Avatar.png" },
+	avatar: { type: String, default: "https://res.cloudinary.com/infinity99/image/upload/v1584729718/avatar/User_Avatar_ub5jq9.png" },
+	avatarId: String,
 	firstName: String,
 	lastName:{ type: String, default: "" },
 	email: { type: String, unique: true,	required: true	},
